21332ff546c9f8190b1a1f8c2e678a05a1b40f9c
karthik-k13 <karthikreddy55015@gmail.com>
2025-08-05 17:35:53 +0530

    commit msg

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("main branch");
+        System.out.println("main branch merged to feature-0001");
 
     }
 }
