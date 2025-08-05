fa7fd13da7a57bf3aacd3af9ee4b738516bfa1fd
karthik-k13 <karthikreddy55015@gmail.com>
2025-08-05 17:42:40 +0530

    Revert "reverting"
    This reverts commit 4c9765a99bde12103d503bf8f36048b953839fe9.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("main branch revert test");
+        System.out.println("main branch merged to feature-0001");
 
     }
 }
