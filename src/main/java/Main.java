04fb7e283a9e664d7cea2c59044cefe482e58283
karthik-k13 <karthik.krishnareddy.mahadi@gmail.com>
2025-08-05 17:45:08 +0530

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,7 +2,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        System.out.println("main branch merged to feature-0001");
+        System.out.println("main branch ");
 
     }
 }
